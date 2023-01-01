d539da39027200a7bf6a2b77481e10776f70b255
pippen11 <91ssm@naver.com>
2023-01-01 13:04:06 +0900

    230101

--- opensea/src/components/main/header/HeaderComponent.jsx
+++ opensea/src/components/main/header/HeaderComponent.jsx
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+// import { ReactComponent as Iconone } from "./icon/magnifying-glass-solid.svg";
+//이렇게 컴포넌트로사용가능
+import searchicon from "./icon/magnifying-glass-solid.svg";
+import { ReactComponent as UserProfile } from "./icon/user-regular.svg";
+// import { ReactComponent as Wallet } from "./icon/wallet-solid.svg";
+import { VscBrowser } from "react-icons/vsc";
+import { ReactComponent as Cart } from "./icon/cart-shopping-solid.svg";
 
 export default function HeaderComponent() {
   return (
@@ -8,25 +15,50 @@ export default function HeaderComponent() {
         <LogoTitle>OpenSea</LogoTitle>
       </MainBarLogo>
       <SearchBarBox>
-        <SearchBar placeholder="Search item,collections,and accounts" />
+        {/* <Iconone width="20" fill="grey" /> */}
+        <ImgPosition>
+          <SearchIcon src={searchicon} />
+        </ImgPosition>
+        <SearchBar placeholder="     Search item,collections,and accounts" />
       </SearchBarBox>
       <DropDownBox>
-        <ul>
+        <DropDownMenu>
           <li>Explore</li>
           <li>Drops</li>
           <li>Stats</li>
           <li>Resources</li>
-        </ul>
+          <li>
+            <UserProfile width="20" />
+          </li>
+          <li>
+            <VscBrowser size={25} />
+          </li>
+          <li>
+            <Cart width="25" />
+          </li>
+        </DropDownMenu>
       </DropDownBox>
     </MainBar>
   );
 }
+const ImgPosition = styled.div`
+  position: fixed;
+
+  left: 229px;
+  top: 21px;
+`;
+
+const SearchIcon = styled.img`
+  width: 15px;
+
+  filter: invert(52%) sepia(10%) saturate(75%) hue-rotate(357deg)
+    brightness(94%) contrast(83%);
+`;
 
 const MainBar = styled.div`
-  /* background-color: rgb(21, 178, 229); */
   font-family: Poppins, sans-serif;
   display: flex;
-  /* align-items: center; */
+  justify-content: space-evenly;
 `;
 
 const MainBarLogo = styled.div`
@@ -36,27 +68,40 @@ const MainBarLogo = styled.div`
 
 const LogoImg = styled.img`
   width: 40px;
-  padding: 10px;
+  padding-left: 50px;
 `;
 
 const LogoTitle = styled.div`
   font-weight: 800;
   font-size: 22px;
+  padding: 15px;
 `;
 
 const SearchBarBox = styled.div`
   width: 50%;
   display: flex;
+  padding-left: 10px;
   align-items: center;
 `;
 
 const SearchBar = styled.input`
   width: 100%;
+  height: 65%;
 
-  padding: 1%;
   font-size: 15px;
   border-radius: 10px;
   border: solid 1px;
 `;
 
-const DropDownBox = styled.div``;
+const DropDownBox = styled.div`
+  width: 40%;
+`;
+
+const DropDownMenu = styled.ul`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 0 0px;
+  list-style: none;
+  font-weight: 800;
+`;
